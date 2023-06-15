ab0888f153a5da102938f69efd10d80ae5adaeb8
Sass <tlsnothercules@gmail.com>
2023-06-15 12:26:26 -0300

    Alterações

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Criar objetos do passeio
+        Pessoa p1 = new Pessoa();
+        Pessoa p2;
+        p2 = new Pessoa();
+        Pessoa pil;
+        pil = new Pessoa();
+        Pessoa bal;
+        bal = new Pessoa();
+        //...
+        p1.nome = "Elliot Page";
+        p1.Cpf = "431.123.111-22";
+        p1.Rg = "12.343.272-1";
+        p1.Endereço = "Rua Japão 101";
+        p1.Tel = "11 99213-2323";
+        p1.TelFam = "11 76521-1221";
+        p1.termoCienciaRisco = true;
+
+        p2.nome = "Bianca Kyao";
+        p2.Cpf = "124.321.079-54";
+        p2.Rg = "54.603.764-4";
+        p2.Endereço = "Rua Japão 101";
+        p2.Tel = "11 76521-1221";
+        p2.TelFam = "11 99123-2323";
+        p2.termoCienciaRisco = true;
+
+        pil.nome = "Gustavo Amaral";
+        pil.Cpf = "233.545.566-32";
+        pil.ANAC = "333.222";
+
+
+
+
     }
+
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,15 @@
 public class Pessoa {
     //Atributos
     String nome;
+    String Cpf; //123.456.789-01
+    String Rg;
+    String Endereço;
+    String ANAC;
+    String Tel;
+    String TelFam;
+    boolean termoCienciaRisco;
+    int numIden;
+    int regularidade;
 
     //Métodos
 }
